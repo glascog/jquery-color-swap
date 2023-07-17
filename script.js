09e427751065872c6de07fb8395f07f77350fa1b
Gabe Glasco <gabeglasco@gmail.com>
2023-07-17 14:08:07 -0500

    added delete button functionality

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ function onReady() {
 
     // Handlers
     $('#generateButton').on('click', handleGenerate);
+    $('main').on('click', '.yellowButton', turnYellow);
+    $('main').on('click', '.deleteButton', handleDelete);
 
 }
 // Create variable for number of divs
-let divCount = 0;
+let divCount = 1;
 
 // Functions
+function handleDelete() {
+    console.log('this is:', $(this));
+    $(this).parent().parent().remove();
+}
+
+function turnYellow(event) {
+    event.preventDefault();
+    console.log('inside turnYellow');
+    $('div').css('background-color', 'yellow');
+}
+
 function handleGenerate(event) {
     event.preventDefault();
     console.log('inside handleGenerate');
@@ -19,8 +32,9 @@ function handleGenerate(event) {
     $('main').append(`
         <div>
             <p>${divCount++}</p>
-            <span><button id="yellowButton">Yellow</button></span>
-            <span><button id="deleteButton">Delete</button></span>
+            <span><button class="yellowButton">Yellow</button></span>
+            <span><button class="deleteButton">Delete</button></span>
         </div>`);
+
 
 }
--- toDo.md
+++ toDo.md
@@ -7,12 +7,12 @@
     [x] jquery: new div appends with every click
     [x] html<p> element inside 
         [x] jquery: shows how many times generate was clicked
-    [] jquery: append two button elements 
-        []yellow
-            [] should change backgound to yellow
-        []delete
-            [] removes parent <div> and that <div>'s children (and delete button)
+    [x] jquery: append two button elements 
+        [x]yellow
+            [x] should change backgound to yellow
+        [x]delete
+            [x] removes parent <div> and that <div>'s children (and delete button)
 
-[] CSS file
-    [] new <div> starts red background-color
+[x] CSS file
+    [x] new <div> starts red background-color
     []
